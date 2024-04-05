74d2963522c1355a6ec686e9f8ceb17589111972
rezadonny <rezadonny@gmail.com>
2024-04-06 05:18:17 +0700

    Modif pada bagian menu atas

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -8,21 +8,24 @@
 </head>
 <body>
     <x-app-layout>
-        <x-slot name="header">
+{{--         <x-slot name="header">
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
-                {{-- {{ __('Dashboard') }} --}}
+                {{-- {{ __('Dashboard') }} 
                 User
             </h2>
-        </x-slot>
+        </x-slot> --}}
 
-        <div class="py-12">
+{{--         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
-                    {{-- <x-welcome /> --}}
+                    {{-- <x-welcome /> 
                     User Halamn ini
                 </div>
             </div>
-        </div>
+        </div> --}}
+
     </x-app-layout>
+
+    <h1>Halaman Home User</h1>
 </body>
 </html>
