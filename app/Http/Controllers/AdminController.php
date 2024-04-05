b7b73853a61b668248afdaf92aa33d3224cb6b01
rezadonny <rezadonny@gmail.com>
2024-04-05 23:20:34 +0700

    membuat route untuk masing2 user_type

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -3,24 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
-use App\Models\User;                                /* menggunakan model tabel user */
-
-use Illuminate\Support\Facades\Auth;                /* untuk mengetahui Auth */
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 
 class AdminController extends Controller
 {
     public function index()
     {
         if (Auth::id()) {
-            $user_type = Auth()->user()->user_type; /* variabel $user_type ditentukan oleh tabel user pada kolom user_type */
+            $user_type = Auth()->user()->user_type;
+
             if ($user_type == 'admin') {
-                return view('admin.index');         /* masuk ke halaman Admin */
+                return view('admin.index');
             } else if ($user_type == 'user') {
-                return view('home.index');          /* Masuk ke halaman User */
+                return view('home.index');
+            } else {
+                return redirect()->back();
             }
-        } else {
-            return redirect()->back();
-        }  /* jika tidak ada isi variabel user_type maka akan tetap dihalaman login */
+        }
     }
 }
