616db06bb8acc8c2fc02989dea373ad102a503a6
rezadonny <rezadonny@gmail.com>
2024-04-06 10:54:57 +0700

    Memisahkan book (content) dan footer

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,176 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 
-  <head>
+<head>
     @include('home.css')
-  </head>
+</head>
 
 <body>
 
-@include('home.header')
+    @include('home.header')
 
-@include('home.main_banner')
-  
-@include('home.category')
-  
+    @include('home.main_banner')
 
-  <div class="currently-market">
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-6">
-          <div class="section-heading">
-            <div class="line-dec"></div>
-            <h2><em>Items</em> Currently In The Market.</h2>
-          </div>
-        </div>
-        <div class="col-lg-6">
-          <div class="filters">
-            <ul>
-              <li data-filter="*"  class="active">All Books</li>
-              <li data-filter=".msc">Popular</li>
-              <li data-filter=".dig">Latest</li>
-              
-            </ul>
-          </div>
-        </div>
-        <div class="col-lg-12">
-          <div class="row grid">
-            <div class="col-lg-6 currently-market-item all msc">
-              <div class="item">
-                <div class="left-image">
-                  <img src="assets/images/book1.webp" alt="" style="border-radius: 20px; min-width: 195px;">
-                </div>
-                <div class="right-content">
-                  <h4>Sarah Maas</h4>
-                  <span class="author">
-                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                    <h6>Robert T Kiyosaki</h6>
-                  </span>
-                  <div class="line-dec"></div>
-                  <span class="bid">
-                    Current Available<br><strong>10</strong><br> 
-                  </span>
-                  <span class="ends">
-                    Total<br><strong>20</strong><br>
-                  </span>
-                  <div class="text-button">
-                    <a href="details.html">View Item Details</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            
+    @include('home.category')
 
-            <div class="col-lg-6 currently-market-item all msc">
-              <div class="item">
-                <div class="left-image">
-                  <img src="assets/images/book2.webp" alt="" style="border-radius: 20px; min-width: 195px;">
-                </div>
-                <div class="right-content">
-                  <h4>Broken Blade</h4>
-                  <span class="author">
-                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                    <h6>Robert T Kiyosaki</h6>
-                  </span>
-                  <div class="line-dec"></div>
-                  <span class="bid">
-                    Current Available<br><strong>10</strong><br> 
-                  </span>
-                  <span class="ends">
-                    Total<br><strong>20</strong><br>
-                  </span>
-                  <div class="text-button">
-                    <a href="details.html">View Item Details</a>
-                  </div>
-                </div>
-              </div>
-            </div>
+    @include('home.book')
 
+    @include('home.footer')
 
+</body>
 
-            <div class="col-lg-6 currently-market-item all msc">
-              <div class="item">
-                <div class="left-image">
-                  <img src="assets/images/book3.webp" alt="" style="border-radius: 20px; min-width: 195px;">
-                </div>
-                <div class="right-content">
-                  <h4>Dwarves</h4>
-                  <span class="author">
-                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                    <h6>Robert T Kiyosaki</h6>
-                  </span>
-                  <div class="line-dec"></div>
-                  <span class="bid">
-                    Current Available<br><strong>10</strong><br> 
-                  </span>
-                  <span class="ends">
-                    Total<br><strong>20</strong><br>
-                  </span>
-                  <div class="text-button">
-                    <a href="details.html">View Item Details</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-
-            <div class="col-lg-6 currently-market-item all msc">
-              <div class="item">
-                <div class="left-image">
-                  <img src="assets/images/book4.webp" alt="" style="border-radius: 20px; min-width: 195px;">
-                </div>
-                <div class="right-content">
-                  <h4>Steven Erikson</h4>
-                  <span class="author">
-                    <img src="assets/images/author.jpg" alt="" style="max-width: 50px; border-radius: 50%;">
-                    <h6>Robert T Kiyosaki</h6>
-                  </span>
-                  <div class="line-dec"></div>
-                  <span class="bid">
-                    Current Available<br><strong>10</strong><br> 
-                  </span>
-                  <span class="ends">
-                    Total<br><strong>20</strong><br>
-                  </span>
-                  <div class="text-button">
-                    <a href="details.html">View Item Details</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <footer>
-    <div class="container">
-      <div class="row">
-        <div class="col-lg-12">
-          <p>Copyright © 2024 <a target="_blank" href="https://www.youtube.com/channel/UCeNYDojo4nU2sbHz1sMsBXw">Web Tech Knowledge
-          &nbsp;&nbsp;
-          Designed by <a title="HTML CSS Templates" rel="sponsored" href="https://www.youtube.com/channel/UCeNYDojo4nU2sbHz1sMsBXw" target="_blank">Web Tech Knowledge</a></p>
-        </div>
-      </div>
-    </div>
-  </footer>
-
-
-  <!-- Scripts -->
-  <!-- Bootstrap core JavaScript -->
-  <script src="vendor/jquery/jquery.min.js"></script>
-  <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
-
-  <script src="assets/js/isotope.min.js"></script>
-  <script src="assets/js/owl-carousel.js"></script>
-
-  <script src="assets/js/tabs.js"></script>
-  <script src="assets/js/popup.js"></script>
-  <script src="assets/js/custom.js"></script>
-
-  </body>
 </html>
