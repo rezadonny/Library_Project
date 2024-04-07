bb51ceb389854db863e6afd43e4a98ef97aec617
rezadonny <rezadonny@gmail.com>
2024-04-07 07:40:13 +0700

    Pisahkan bagian footer ke dalam footer.blade

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,36 +2,21 @@
 <html>
 
 <head>
-@include('admin.css')
+    @include('admin.css')
 </head>
 
 <body>
     @include('admin.header')
     <div class="d-flex align-items-stretch">
         <!-- Sidebar Navigation-->
-@include('admin.sidebar')
+        @include('admin.sidebar')
         <!-- Sidebar Navigation end-->
         @include('admin.body')
-            <footer class="footer">
-                <div class="footer__block block no-margin-bottom">
-                    <div class="container-fluid text-center">
-                        <!-- Please do not remove the backlink to us unless you support us at https://bootstrapious.com/donate. It is part of the license conditions. Thank you for understanding :)-->
-                        <p class="no-margin-bottom">2018 &copy; Your company. Download From <a target="_blank"
-                                href="https://templateshub.net">Templates Hub</a>.</p>
-                    </div>
-                </div>
-            </footer>
-        </div>
-    </div>
-    <!-- JavaScript files-->
-    <script src="admin/vendor/jquery/jquery.min.js"></script>
-    <script src="admin/vendor/popper.js/umd/popper.min.js"></script>
-    <script src="admin/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="admin/vendor/jquery.cookie/jquery.cookie.js"></script>
-    <script src="admin/vendor/chart.js/Chart.min.js"></script>
-    <script src="admin/vendor/jquery-validation/jquery.validate.min.js"></script>
-    <script src="admin/js/charts-home.js"></script>
-    <script src="admin/js/front.js"></script>
+
+
+
+
+        @include('admin.footer')
 </body>
 
 </html>
